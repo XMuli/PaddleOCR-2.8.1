d65225237e5b80026eca6d549edb19748ec65663
XMuli <xmulitech@gmail.com>
2024-08-14 23:57:47 +0800

    feat: modify args.cpp

--- deploy/cpp_infer/src/args.cpp
+++ deploy/cpp_infer/src/args.cpp
@@ -23,13 +23,13 @@ DEFINE_int32(cpu_threads, 10, "Num of threads with CPU.");
 DEFINE_bool(enable_mkldnn, false, "Whether use mkldnn with CPU.");
 DEFINE_string(precision, "fp32", "Precision be one of fp32/fp16/int8");
 DEFINE_bool(benchmark, false, "Whether use benchmark.");
-DEFINE_string(output, "./output/", "Save benchmark log path.");
-DEFINE_string(image_dir, "", "Dir of input image.");
+DEFINE_string(output, "./output", "Save benchmark log path.");
+DEFINE_string(image_dir, "./imgs", "Dir of input image.");
 DEFINE_string(
     type, "ocr",
     "Perform ocr or structure, the value is selected in ['ocr','structure'].");
 // detection related
-DEFINE_string(det_model_dir, "", "Path of det inference model.");
+DEFINE_string(det_model_dir, "./infer/ch_PP-OCRv4_det_infer", "Path of det inference model.");
 DEFINE_string(limit_type, "max", "limit_type of input image.");
 DEFINE_int32(limit_side_len, 960, "limit_side_len of input image.");
 DEFINE_double(det_db_thresh, 0.3, "Threshold of det_db_thresh.");
@@ -44,9 +44,9 @@ DEFINE_string(cls_model_dir, "", "Path of cls inference model.");
 DEFINE_double(cls_thresh, 0.9, "Threshold of cls_thresh.");
 DEFINE_int32(cls_batch_num, 1, "cls_batch_num.");
 // recognition related
-DEFINE_string(rec_model_dir, "", "Path of rec inference model.");
+DEFINE_string(rec_model_dir, "./infer/ch_PP-OCRv4_rec_infer", "Path of rec inference model.");
 DEFINE_int32(rec_batch_num, 6, "rec_batch_num.");
-DEFINE_string(rec_char_dict_path, "../../ppocr/utils/ppocr_keys_v1.txt",
+DEFINE_string(rec_char_dict_path, "./infer/ppocr_keys_v1.txt",
               "Path of dictionary.");
 DEFINE_int32(rec_img_h, 48, "rec image height");
 DEFINE_int32(rec_img_w, 320, "rec image width");
